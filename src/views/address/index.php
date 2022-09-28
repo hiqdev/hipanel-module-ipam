c8776b0e8432db74773f4f5a101de2818d2147fc
gadezist <andrybitko@gmail.com>
2022-09-28 16:29:15 +0300

    HP-1037 Remove PJAX form HiPanel

--- src/views/address/index.php
+++ src/views/address/index.php
@@ -4,15 +4,16 @@ use hipanel\models\IndexPageUiOptions;
 use hipanel\modules\ipam\grid\AddressGridView;
 use hipanel\modules\ipam\models\Address;
 use hipanel\widgets\IndexPage;
-use hipanel\widgets\Pjax;
 use hiqdev\higrid\representations\RepresentationCollection;
-use yii\data\ArrayDataProvider;
+use hiqdev\hiart\ActiveDataProvider;
 use yii\helpers\Html;
 
-/** @var Address $model */
-/** @var RepresentationCollection $representationCollection */
-/** @var ArrayDataProvider $dataProvider */
-/** @var IndexPageUiOptions $uiModel */
+/**
+ * @var Address $model
+ * @var RepresentationCollection $representationCollection
+ * @var ActiveDataProvider $dataProvider
+ * @var IndexPageUiOptions $uiModel
+ */
 
 $this->title = Yii::t('hipanel.ipam', 'IP Addresses');
 $this->params['subtitle'] = array_filter(Yii::$app->request->get($model->formName(), [])) ? Yii::t('hipanel', 'filtered list') : Yii::t('hipanel', 'full list');
@@ -20,37 +21,35 @@ $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 
-<?php Pjax::begin(array_merge(Yii::$app->params['pjax'], ['enablePushState' => true])) ?>
-    <?php $page = IndexPage::begin(compact('model', 'dataProvider')) ?>
+<?php $page = IndexPage::begin(['model' => $model, 'dataProvider' => $dataProvider]) ?>
 
-        <?php $page->beginContent('main-actions') ?>
-            <?= Html::a(Yii::t('hipanel.ipam', 'Add a new address'), 'create', ['class' => 'btn btn-sm btn-success']) ?>
-        <?php $page->endContent() ?>
+    <?php $page->beginContent('main-actions') ?>
+        <?= Html::a(Yii::t('hipanel.ipam', 'Add a new address'), 'create', ['class' => 'btn btn-sm btn-success']) ?>
+    <?php $page->endContent() ?>
 
-        <?php $page->beginContent('sorter-actions') ?>
-            <?php if (!$dataProvider instanceof ArrayDataProvider): ?>
-                <?= $page->renderSorter([
-                    'attributes' => ['ip']
-                ]) ?>
-            <?php endif ?>
-        <?php $page->endContent() ?>
+    <?php $page->beginContent('sorter-actions') ?>
+        <?php if (!$dataProvider instanceof ArrayDataProvider): ?>
+            <?= $page->renderSorter([
+                'attributes' => ['ip']
+            ]) ?>
+        <?php endif ?>
+    <?php $page->endContent() ?>
 
-        <?php $page->beginContent('bulk-actions') ?>
-            <?= $page->renderBulkDeleteButton('delete')?>
-        <?php $page->endContent() ?>
+    <?php $page->beginContent('bulk-actions') ?>
+        <?= $page->renderBulkDeleteButton('delete')?>
+    <?php $page->endContent() ?>
 
-        <?php $page->beginContent('table') ?>
-            <?php $page->beginBulkForm() ?>
-                <?= AddressGridView::widget([
-                    'boxed' => false,
-                    'dataProvider' => $dataProvider,
-                    'filterModel' => $model,
-                    'columns' => $representationCollection->getByName($uiModel->representation)->getColumns(),
-                    'rowOptions' => static fn(Address $prefix, $key): array => [
-                        'class' => sprintf("%s", $prefix->isSuggested() ? 'success' : ''),
-                    ],
-                ]) ?>
-            <?php $page->endBulkForm() ?>
-        <?php $page->endContent() ?>
-    <?php $page::end() ?>
-<?php Pjax::end() ?>
+    <?php $page->beginContent('table') ?>
+        <?php $page->beginBulkForm() ?>
+            <?= AddressGridView::widget([
+                'boxed' => false,
+                'dataProvider' => $dataProvider,
+                'filterModel' => $model,
+                'columns' => $representationCollection->getByName($uiModel->representation)->getColumns(),
+                'rowOptions' => static fn(Address $prefix, $key): array => [
+                    'class' => sprintf("%s", $prefix->isSuggested() ? 'success' : ''),
+                ],
+            ]) ?>
+        <?php $page->endBulkForm() ?>
+    <?php $page->endContent() ?>
+<?php $page::end() ?>
--- src/views/prefix/index.php
+++ src/views/prefix/index.php
@@ -3,18 +3,19 @@
 use hipanel\models\IndexPageUiOptions;
 use hipanel\modules\ipam\grid\PrefixGridLegend;
 use hipanel\modules\ipam\grid\PrefixGridView;
+use hipanel\modules\ipam\grid\PrefixRepresentations;
 use hipanel\modules\ipam\models\Aggregate;
 use hipanel\widgets\gridLegend\GridLegend;
 use hipanel\widgets\IndexPage;
-use hipanel\widgets\Pjax;
-use hiqdev\higrid\representations\RepresentationCollection;
 use yii\data\ArrayDataProvider;
 use yii\helpers\Html;
 
-/** @var Aggregate $model */
-/** @var RepresentationCollection $representationCollection */
-/** @var ArrayDataProvider $dataProvider */
-/** @var IndexPageUiOptions $uiModel */
+/**
+ * @var Aggregate $model
+ * @var PrefixRepresentations $representationCollection
+ * @var ArrayDataProvider $dataProvider
+ * @var IndexPageUiOptions $uiModel
+ */
 
 $this->title = Yii::t('hipanel.ipam', 'Prefixes');
 $this->params['subtitle'] = array_filter(Yii::$app->request->get($model->formName(), [])) ? Yii::t('hipanel', 'filtered list') : Yii::t('hipanel', 'full list');
@@ -22,38 +23,36 @@ $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 
-<?php Pjax::begin(array_merge(Yii::$app->params['pjax'], ['enablePushState' => true])) ?>
-    <?php $page = IndexPage::begin(compact('model', 'dataProvider')) ?>
+<?php $page = IndexPage::begin(['model' => $model, 'dataProvider' => $dataProvider]) ?>
 
-        <?php $page->beginContent('legend') ?>
-            <?= GridLegend::widget(['legendItem' => new PrefixGridLegend($model)]) ?>
-        <?php $page->endContent() ?>
+    <?php $page->beginContent('legend') ?>
+        <?= GridLegend::widget(['legendItem' => new PrefixGridLegend($model)]) ?>
+    <?php $page->endContent() ?>
 
-        <?php $page->beginContent('main-actions') ?>
-            <?= Html::a(Yii::t('hipanel.ipam', 'Add a new prefix'), 'create', ['class' => 'btn btn-sm btn-success']) ?>
-        <?php $page->endContent() ?>
+    <?php $page->beginContent('main-actions') ?>
+        <?= Html::a(Yii::t('hipanel.ipam', 'Add a new prefix'), 'create', ['class' => 'btn btn-sm btn-success']) ?>
+    <?php $page->endContent() ?>
 
-        <?php $page->beginContent('sorter-actions') ?>
-            <?= $page->renderSorter([
-                'attributes' => ['ip']
+    <?php $page->beginContent('sorter-actions') ?>
+        <?= $page->renderSorter([
+            'attributes' => ['ip']
+        ]) ?>
+    <?php $page->endContent() ?>
+
+    <?php $page->beginContent('bulk-actions') ?>
+        <?= $page->renderBulkButton('update', Yii::t('hipanel', 'Update'))?>
+        <?= $page->renderBulkDeleteButton('delete')?>
+    <?php $page->endContent() ?>
+
+    <?php $page->beginContent('table') ?>
+        <?php $page->beginBulkForm() ?>
+            <?= PrefixGridView::widget([
+                'boxed' => false,
+                'dataProvider' => $dataProvider,
+                'filterModel' => $model,
+                'columns' => $representationCollection->getByName($uiModel->representation)->getColumns(),
+                'colorize' => true,
             ]) ?>
-        <?php $page->endContent() ?>
-
-        <?php $page->beginContent('bulk-actions') ?>
-            <?= $page->renderBulkButton('update', Yii::t('hipanel', 'Update'))?>
-            <?= $page->renderBulkDeleteButton('delete')?>
-        <?php $page->endContent() ?>
-
-        <?php $page->beginContent('table') ?>
-            <?php $page->beginBulkForm() ?>
-                <?= PrefixGridView::widget([
-                    'boxed' => false,
-                    'dataProvider' => $dataProvider,
-                    'filterModel' => $model,
-                    'columns' => $representationCollection->getByName($uiModel->representation)->getColumns(),
-                    'colorize' => true,
-                ]) ?>
-            <?php $page->endBulkForm() ?>
-        <?php $page->endContent() ?>
-    <?php $page::end() ?>
-<?php Pjax::end() ?>
+        <?php $page->endBulkForm() ?>
+    <?php $page->endContent() ?>
+<?php $page::end() ?>
